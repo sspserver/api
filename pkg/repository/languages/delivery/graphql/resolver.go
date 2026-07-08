664831e10ff9dc6234abdaabe4687c8027a2b0e2
Dmitry Ponomarev <demdxx@gmail.com>
2026-07-08 20:04:50 +0200

    Fix lint warnings in resolver.go and directives files.

--- pkg/repository/languages/delivery/graphql/resolver.go
+++ pkg/repository/languages/delivery/graphql/resolver.go
@@ -2,12 +2,12 @@ package graphql
 
 import (
 	"context"
+	"slices"
 	"strings"
 
 	"github.com/demdxx/xtypes"
 	"github.com/geniusrabbit/adcorelib/i18n/languages"
 	gqlmodels "github.com/sspserver/api/pkg/server/graphql/models"
-	"golang.org/x/exp/slices"
 )
 
 type QueryResolver struct {
@@ -23,17 +23,15 @@ func (r *QueryResolver) Languages(ctx context.Context, filter *gqlmodels.LangLis
 	if filter != nil {
 		list = xtypes.Slice[languages.Language](languages.Languages).
 			Filter(func(lang languages.Language) bool {
-				if len(filter.ID) > 0 && slices.Contains(filter.ID, uint64(lang.ID)) {
+				if slices.Contains(filter.ID, uint64(lang.ID)) {
 					return true
 				}
-				if len(filter.Iso2) > 0 && slices.Contains(filter.Iso2, string(lang.Code[:])) {
+				if slices.Contains(filter.Iso2, string(lang.Code[:])) {
 					return true
 				}
-				if len(filter.Name) > 0 {
-					nlw := strings.ToLower(lang.Name)
-					if slices.ContainsFunc(filter.Name, func(v string) bool { return nlw == strings.ToLower(v) }) {
-						return true
-					}
+				lngLow := strings.ToLower(lang.Name)
+				if slices.ContainsFunc(filter.Name, func(v string) bool { return lngLow == strings.ToLower(v) }) {
+					return true
 				}
 				if len(filter.NativeName) > 0 {
 					nlw := strings.ToLower(lang.NativeName)
